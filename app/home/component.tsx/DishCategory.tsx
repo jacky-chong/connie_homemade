03c4bd955d7e471184661348746555cf85981596
Jacky <jackyklchong@strateqgroup.com>
2024-12-31 17:59:20 +0800

    add auto scroll to desire type when select category

--- app/home/component.tsx/DishCategory.tsx
+++ app/home/component.tsx/DishCategory.tsx
@@ -1,6 +1,6 @@
 // components/DishCategory.tsx
 
-import React from "react";
+import React, { forwardRef } from "react";
 import { List } from "antd";
 import { DishType } from "../type/DishType";
 import DishItem from "@/app/component/DishItem";
@@ -11,27 +11,29 @@ type DishCategoryProps = {
   showPhotos: (code: string) => void;
 };
 
-const DishCategory = ({
-  dishCategory,
-  allPhotos,
-  showPhotos,
-}: DishCategoryProps) => {
-  return (
-    <div className="mt-4">
-      <p className="text-xl font-semibold">
-        {dishCategory.name}{" "}
-        <span className="text-gray-500">({dishCategory.data.length})</span>
-      </p>
+const DishCategory = forwardRef<HTMLDivElement, DishCategoryProps>(
+  ({ dishCategory, allPhotos, showPhotos }, ref) => {
+    return (
+      <div ref={ref} className="mt-4 scroll-mt-36">
+        <p className="text-xl font-semibold">
+          {dishCategory.name}{" "}
+          <span className="text-gray-500">({dishCategory.data.length})</span>
+        </p>
 
-      <List
-        itemLayout="horizontal"
-        dataSource={dishCategory.data}
-        renderItem={(item) => (
-          <DishItem dish={item} allPhotos={allPhotos} showPhotos={showPhotos} />
-        )}
-      />
-    </div>
-  );
-};
+        <List
+          itemLayout="horizontal"
+          dataSource={dishCategory.data}
+          renderItem={(item) => (
+            <DishItem
+              dish={item}
+              allPhotos={allPhotos}
+              showPhotos={showPhotos}
+            />
+          )}
+        />
+      </div>
+    );
+  }
+);
 
 export default DishCategory;
